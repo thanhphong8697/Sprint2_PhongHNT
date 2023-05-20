5cfdc05f3411cf73870b4af8e214988e866ce60b
thanhphong8697 <thanhphong861997@gmail.com>
2023-05-20 07:17:49 +0700

    first commit

---
+++ demo/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginComponent from './LoginComponent';
+
+function App() {
+  return (
+    <>
+    <LoginComponent/>
+    </>
+  );
+}
+
+export default App;
